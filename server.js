c4966686b2b69f05a629a537e3f773ada3c6ddaa
Michał Swoboda <michal.swoboda@interia.pl>
2023-01-13 21:52:24 +0100

    Create and join game thgrough console

--- server.js
+++ server.js
@@ -1,10 +1,84 @@
-let express = require('express')
-let app = express()
+const express = require('express')
+const WebSocket = require('ws')
+const app = express()
+const wss = new WebSocket.Server({ port: 8081 })
+
+let clients = {}
+let games = {}
+
+function generateId(x){
+    let chars, len, dict, ret
+    if (x == "client"){
+        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+        len = 10
+        dict = clients
+    }
+    else if (x == "game"){
+        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        len = 6
+        dict = games
+    }
+    else{
+        return "Error"
+    }
+    do{
+        ret = ""
+        for (let j = 0; j < len; j++) {
+            ret += chars[Math.floor(Math.random()*chars.length)]
+        }
+    }
+    while(dict.hasOwnProperty(ret))
+    return ret
+}
+
+wss.on('connection', function connection(ws) {
+    let nickname, clientId, gameId
+    ws.on("close",()=>{
+        clients.hasOwnProperty(clientId)?delete clients.clientId:""
+        if (games.hasOwnProperty(gameId)){
+            games[gameId].players.splice(games[gameId].players.indexOf(clientId),1)
+            if (games[gameId].players.length == 0){
+                delete games[gameId]
+            }
+        }
+    })
+    ws.on("message",(msg)=>{
+        let data = JSON.parse(msg)
+        switch(data.type){
+            case "createGame":
+                nickname = data.nickname
+                clientId = generateId("client")
+                clients[clientId] = {ws: ws, nickname: nickname}
+                gameId = generateId("game")
+                games[gameId] = {players: [clientId], maxPlayers: data.maxPlayers, status: "waitingRoom"}
+                ws.send(JSON.stringify({type: "gameCreated", gameId: gameId, playerId: clientId}))
+                break
+            case "joinGame":
+                if (games.hasOwnProperty(data.code)){
+                    nickname = data.nickname
+                    clientId = generateId("client")
+                    clients[clientId] = {ws: ws, nnickname: nickname}
+                    let players = {}
+                    games[data.code].players.forEach(element => {
+                        players[element] = {nickname: clients[element].nickname, playerId: clients[element].playerId}
+                        clients[element].ws.send(JSON.stringify({type: "newPlayer", playerId: clientId, nickname: nickname}))
+                    })
+                    ws.send(JSON.stringify({type: "gameJoined", playerId: clientId, players: players}))
+                    games[data.code].players.push(clientId)
+                }
+                else{
+                    ws.send(JSON.stringify({type: "gameNotFound"}))
+                    ws.close()
+                }
+                break
+        }
+    })
+})
 
 app.use(express.static(__dirname+'/site')) 
 
 app.get("/",(req,res)=>{
-    res.sendFile(__dirname+"/site/index.html")  
+    res.sendFile(__dirname+"/site/index.html")
 })
 
 app.get("/test",(req,res)=>{
--- site/script.js
+++ site/script.js
@@ -1,5 +1,7 @@
 let main = document.querySelector("main")
-let serverInterval = null
+let clientId = null
+let gameId = null
+let ws = null
 
 function menu(){
     main.innerHTML = `
@@ -10,18 +12,94 @@ function menu(){
                 <div onclick="joinGamePopup()">Join game</div>
             </div>
         </div>
+        <div id="darkScreen" style="display:none;"></div>
         <div id="popup"></div>
     `
 }
 function createGamePopup(){
-    //
+    document.querySelector("#darkScreen").style.display = "block"
+    document.querySelector("#popup").innerHTML = `
+        <h1>Create game</h1>
+        <table>
+        <tr>
+            <td>Nickname:</td>
+            <td><input type="text" id="nickname"></td>
+        </tr>
+        <tr>
+            <td>Max players:</td>
+            <td>
+                <select id="players">
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                </select><br>
+            </td>
+        </tr>
+        </table>
+        <button onclick="createGame()">Create game</button>
+        <button onclick='closePopup()'>Cancel</button>
+    `
 }
 function joinGamePopup(){
+    document.querySelector("#darkScreen").style.display = "block"
     document.querySelector("#popup").innerHTML = `
-        <h1>Wpisz kod poczekalni</h1>
-        <input type="text">
+        <h1>Enter code</h1>
+        <table>
+        <tr>
+            <td>Nickname:</td>
+            <td><input type="text" id="nickname"></td>
+        </tr>
+        <tr>
+            <td>Code:</td>
+            <td><input type="text" id="code"></td>
+        </tr>
+        </table>
         <button onclick="joinGame()">Dołącz</button>
+        <button onclick='closePopup()'>Cancel</button>
     `
+}
+function closePopup(){
+    document.querySelector("#popup").innerHTML = ""
+    document.querySelector("#darkScreen").style.display = "none"
+}
+function createGame(){
+    ws = new WebSocket("ws://localhost:8081")
+    ws.onmessage = (msg)=>{
+        let data = JSON.parse(msg.data)
+        /*switch(data.type){
+            case "gameCreated":
+                clientId = data.playerId
+                gameId = data.gameId
+                break
+        }*/
+        console.log(data)
+    }
+    ws.onopen = ()=>{
+        ws.send(JSON.stringify({type: "createGame", nickname: document.querySelector("#nickname").value , maxPlayers: document.querySelector("#players").value}))
+    }
+}
+function joinGame(){
+    ws = new WebSocket("ws://localhost:8081")
+    ws.onmessage = (msg)=>{
+        let data = JSON.parse(msg.data)
+        console.log(data)
+        switch(data.type){
+            case "gameJoined":
+                break
+            case "gameNotFound":
+                // do something
+                ws.close()
+                break
+        }
+    }
+    ws.onopen = ()=>{
+        ws.send(JSON.stringify({type: "joinGame", nickname: document.querySelector("#nickname").value, code: document.querySelector("#code").value}))
+    }
 }
 window.onload=()=>{
     menu()
